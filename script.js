fba5114eda2faa451da2dffe8f157868e8294800
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-05-30 19:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-// function DecimalToBinary(num) {
-//   let binary = '';
-//   while(num > 0) {
-//     binary = (num % 2) + binary;
-//     num = Math.floor(num / 2);
-//   }
-  // return binary;
-// }
-// console.log(DecimalToBinary(10));
+ function DecimalToBinary(num) {
+    return num.toString(2);
+ }
 
-function decimalToBinary(decimal) {
+ let result=(DecimalToBinary(10));
+console.log(result);
 
-  let binary = "";
-  if (decimal === 0) {
-    return "0";
-  }
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-  return binary;
-}
+
+  
